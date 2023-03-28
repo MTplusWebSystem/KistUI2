d65515f3bd493fa196476281a99160dd2888e907
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2023-03-27 22:25:16 -0400

    Create elementos.js

---
+++ Categoria/cartões/sombra/elementos.js
@@ -0,0 +1,18 @@
+const Teg1 = `
+ <div class="container">
+   <div class="text">
+     <div class="img">
+       <slot name="img"></slot>
+     </div> 
+     <slot name="title"></slot>
+     <slot name="description"></slot>
+   </div> 
+   <div id="category">
+     <slot name="value"></slot>
+     <span></span>
+   </div> 
+  </div> 
+`
+
+export {
+  Teg1}
