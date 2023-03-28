4627fd878dc74ab238d3854203b2d94d459b3fbe
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2023-03-27 22:27:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-import "./pricing-page/pricingPage1.js";
-import "./Card/cardstyle1.js";
-import "./NavButton/navbutton.js";
+import {ComponentsPages} from"./category/PricingPages/Pages1.js"
+import {ComponentsBars} from"./category/NavBars/NavBars1.js"
+import {ComponentsCards} from"./category/Cards/Cards1.js"
+  
+ 
+customElements.define('pricing-page1',ComponentsPages)
+customElements.define('nav-bar1',ComponentsBars)
+customElements.define('cards-1',ComponentsCards)
