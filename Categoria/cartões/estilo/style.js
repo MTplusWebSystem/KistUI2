de23723500cd6ca329a9491705a0118e79a738f3
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2023-03-27 22:24:14 -0400

    Create style.js

---
+++ Categoria/cartões/estilo/style.js
@@ -0,0 +1,41 @@
+const Cards1 =`
+<style>
+  .container{
+    height:60vh;
+    width:90vw;
+    background-color:#ddd;
+    border-radius: 15px;
+    display: flex;
+  }
+  .text{
+    width:80%;
+    text-align: center;
+  }
+ .img{
+   margin-top:4vh;
+   margin-left:6vw;
+   height: 80px;
+   width:80px;
+   
+ }
+#category{
+  position: relative;
+  text-align: center;
+  width:20%;
+  height:80px;
+  border-radius: 0px 15px 0px 0px;
+  background-color: #FFBA00;
+}
+span{
+  position: absolute;
+  margin-left: -50%;
+  width:100%;
+  border-radius:0px 0px 15px 0px ;
+  height:49.3vh;
+  background-color: #FFD633;
+}
+  </style>
+    `
+
+export {
+  Cards1}
