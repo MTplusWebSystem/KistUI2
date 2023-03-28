0ea2e57554a68b8839f8d62a68bd79c49ea19583
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2023-03-27 22:30:21 -0400

    Create style.js

---
+++ Categoria/Barras de navegação/estilo/style.js
@@ -0,0 +1,28 @@
+const NavBars1 =`
+  <style>
+   .container{
+      background-color: #fff;
+
+    width:100vw;
+
+    border-radius: 0px 0px 15px 15px;
+  }
+  .container ul{
+    position: relative;
+    padding: 0;
+    height: 7vh;
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+  }
+  .container li{
+    margin: 5px;
+  }
+  .container a{
+    cursor:none;
+  }
+  </style>
+    `
+
+export {
+  NavBars1}
